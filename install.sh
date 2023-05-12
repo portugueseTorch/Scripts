3fc2aaeea236e1f218be941719e14425454fefd7
Gabriel Da Cruz Tocha <gda-cruz@c1r6s3.42lisboa.com>
2023-05-12 15:43:57 +0100

    installer started

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Go to the home directory and clone Scripts repo
+cd "$HOME" || exit
+git clone https://github.com/portugueseTorch/CPP_Scripts || exit
+
+# Create a new directory in home to store the scripts
+mkdir .scripts || exit
+cp CPP_Scripts/*.py ./.scripts
+
+# Creating the alias - ADD YOUR NEW ONES HERE
+RC_FILE="$HOME/.zshrc"
+
+# New module alias
+if ! grep "new=" "$RC_FILE" &> /dev/null; then
+	echo "new alias not present"
+	printf "\nalias new=\"python3 %s/.scripts/new_module.py\"\n" "$HOME" >> "$RC_FILE"
+
+# Delete git repo
+rm -rf CPP_Scripts
---
+++ new_module.py
@@ -0,0 +1,42 @@
+import sys
+import shutil
+import os
+
+if len(sys.argv) < 3:
+	print("USAGE: newex __MODULE_NAME__ __NUMBER_EXERECISES__")
+	quit()
+
+
+module_folder = sys.argv[1]
+list = [ module_folder ]
+
+if (sys.argv[2]).isdigit():
+	r = int(sys.argv[2])
+else:
+	print("__NUMBER_EXERCISES__ must be a number")
+	quit()
+
+for i in range(r):
+		ex_dir = module_folder + "/ex0" + str(i)
+		list.append(ex_dir)
+
+for item in list:
+	try:
+		os.mkdir(item)
+	except OSError:
+		print("File already exists")
+		quit()
+	if list.index(item) == 0:
+		continue
+	try:
+		shutil.copy("Makefile", item)
+	except:
+		print("Makefile does not exist")
+		quit()
+	sub_list = [ item + "/src", item + "/inc" ]
+	for subitem in sub_list:
+		try:
+			os.mkdir(subitem)
+		except OSError:
+			print("File already exists")
+			quit()
